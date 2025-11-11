b2a20c8ea0611ca41852d5b4be1681381cd0a806
RolandMario <rolandmario2@gmail.com>
2025-11-11 23:28:41 +0100

    remove bodyParser

--- Backend/server.js
+++ Backend/server.js
@@ -20,6 +20,7 @@ connectDB();
 const corsOptions = {
   origin: [
     "http://localhost:5173",
+    "https://welli-record.vercel.app"
   ],
   credentials: true,
   methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
@@ -29,7 +30,8 @@ const corsOptions = {
 app.use(express.json({ limit: "10mb" }));
 app.use(morgan("dev"));
 app.use(cors(corsOptions));
-app.use(bodyParse.json({ limit: "10mb" }));
+// app.use(bodyParse.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 
