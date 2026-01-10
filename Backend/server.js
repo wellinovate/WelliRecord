d6b5f6d44a3f5ad23606c3fbbdf9d50c1428f58c
RolandMario <rolandmario2@gmail.com>
2026-01-10 11:04:01 +0100

    add deployed origin-2

--- Backend/server.js
+++ Backend/server.js
@@ -21,6 +21,7 @@ const corsOptions = {
   origin: [
     "http://localhost:3000",
     "https://wellirecords.vercel.app",
+    "https://wellirecord.com",
     "https://welli-record.vercel.app"
   ],
   credentials: true,
